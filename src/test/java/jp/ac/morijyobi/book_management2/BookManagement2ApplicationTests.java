b880ba573879af2042f4703bb03f9309fb3438ea
youhei-takahashi <y.takahashi@morijyobi.ac.jp>
2023-11-15 14:31:17 +0900

    init

---
+++ src/test/java/jp/ac/morijyobi/book_management2/BookManagement2ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.morijyobi.book_management2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagement2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
